ebb4cb3f3753aa3f965a4d395063071d3f86d810
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-04-13 15:51:49 +0200

    spring init

---
+++ src/main/java/pl/lodz/p/it/zzpj/dogs/DogsApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.zzpj.dogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsApplication.class, args);
+	}
+
+}
