7bfa3f3f1882fa86f4d47c862251471c2042b73a
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-05-08 23:16:24 +0200

    searching dogs by breed

--- src/main/java/pl/lodz/p/it/zzpj/dogs/controllers/DogController.java
+++ src/main/java/pl/lodz/p/it/zzpj/dogs/controllers/DogController.java
@@ -4,14 +4,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import pl.lodz.p.it.zzpj.dogs.model.Dog;
 import pl.lodz.p.it.zzpj.dogs.repositories.DogRepository;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class DogController {
@@ -23,6 +28,8 @@ public class DogController {
     @GetMapping("/dog")
     public String addDog(Model model) throws JsonProcessingException {
         ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl, String.class);
+        log.error(response.toString());
+
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(response.getBody());
         JsonNode message = root.path("message");
@@ -32,6 +39,27 @@ public class DogController {
         dog.setBreed(imageUrl.substring(30, imageUrl.lastIndexOf("/")));
         dogRepository.insert(dog);
         model.addAttribute("image", dog.getUrl());
+        return "greeting";
+    }
+
+    @GetMapping("/dog/{breed}")
+     public String addDogByBreed(Model model, @PathVariable String breed) throws JsonProcessingException {
+
+        log.error(String.format("https://dog.ceo/api/breed/%s/images/random",breed));
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                String.format("https://dog.ceo/api/breed/%s/images/random",breed),String.class);
+
+        log.error(response.toString());
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = null;
+        root = mapper.readTree(response.getBody());
+        JsonNode message = root.path("message");
+        String imageUrl = message.toString().replace("\"", "");
+        Dog dog = new Dog();
+        dog.setUrl(imageUrl);
+        dog.setBreed(imageUrl.substring(30, imageUrl.lastIndexOf("/")));
+        dogRepository.insert(dog);
+        model.addAttribute("imageByBreed", dog.getUrl());
         return "greeting";
     }
 }
