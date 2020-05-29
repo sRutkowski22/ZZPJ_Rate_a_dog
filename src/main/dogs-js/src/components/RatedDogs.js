13bb6b4a83e3d8b2df9081778f9d6e8d689442b3
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-05-29 22:37:55 +0200

    Reviews list in table

--- src/main/dogs-js/src/components/RatedDogs.js
+++ src/main/dogs-js/src/components/RatedDogs.js
@@ -1,36 +1,70 @@
 import React, {Component} from 'react';
 import paginationFactory from "react-bootstrap-table2-paginator";
-import BootstrapTable from "react-bootstrap-table-next"
+import BootstrapTable from "react-bootstrap-table-next";
+import {Link} from "react-router-dom";
+import axios from "axios";
+import {Image} from "react-bootstrap";
+import row from "react-select";
 export default class RatedDogs extends Component{
 
     constructor(props) {
         super(props);
         this.state = {
-            dogs: [],
+            currentRow: 0,
+            reviews: [],
             columns: [{
-                dataField: "Dog",
-                text: "Dog",
-                sort: true
+                dataField: "url",
+                text: "url",
+                sort: true,
+                isDummyField: true,
+                formatter: this.displayImage
             }, {
-                dataField: "Breed",
+                dataField: "breed",
                 text: "Breed",
                 sort: true
 
             }, {
-                dataField: "Rating",
+                dataField: "rating",
                 text: "Rating",
                 sort: true
             }, {
-                dataField: "Username",
+                dataField: "username",
                 text: "Username",
                 sort: true
             },{
-                dataField: "Date",
+                dataField: "creationDate",
                 text: "Date",
                 sort: true
             }]
         }
     }
+
+    displayImage = () => {
+
+            console.log("herb")
+            return <img src={this.state.reviews[8]["url"]} width={130} height={130}></img>;
+
+    };
+
+
+    componentDidMount = () => {
+        axios.get("/reviews")
+            .then(response =>{
+                let tempReviews = response.data;
+                for(let review of tempReviews) {
+                    let link = review["url"];
+
+                }
+
+                this.setState({
+                    reviews: tempReviews
+                })
+                for(let review of tempReviews){
+                    console.log(review)
+                }
+            })
+    }
+
     renderTable = () => {
         const sizes = [{
             text: "10", value: 10
@@ -43,7 +77,10 @@ export default class RatedDogs extends Component{
         }];
         return(
             <div>
-                <BootstrapTable keyField="id" data={this.state.dogs} columns={this.state.columns} pagination={paginationFactory({sizePerPageList: sizes})} bootstrap4={true}/>
+                <BootstrapTable keyField="id" data={this.state.reviews}
+                                columns={this.state.columns}
+                                pagination={paginationFactory({sizePerPageList: sizes})}
+                                bootstrap4={true}/>
             </div>
         )
     }
