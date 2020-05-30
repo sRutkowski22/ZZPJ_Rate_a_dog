6ae6c38ba187588baa563b0d44bc13dbd785a263
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-05-30 12:11:35 +0200

    Use firefox driver, remove hardcoded url

--- src/test/java/pl/lodz/p/it/zzpj/dogs/SeleniumTestsIT.java
+++ src/test/java/pl/lodz/p/it/zzpj/dogs/SeleniumTestsIT.java
@@ -1,6 +1,7 @@
 package pl.lodz.p.it.zzpj.dogs;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,16 +10,21 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 
+@Slf4j
 @SpringBootTest(webEnvironment = DEFINED_PORT)
 @Import(TestMongoConfiguration.class)
 @ExtendWith(TestSuiteExtension.class)
@@ -26,27 +32,35 @@ public class SeleniumTestsIT {
 
     private WebDriver driver;
     private WebDriverWait wait;
+    private String url;
 
     @BeforeEach
     public void setUp() {
-        WebDriverManager.getInstance(ChromeDriver.class).setup();
-        ChromeOptions options = new ChromeOptions();
+        WebDriverManager.getInstance(FirefoxDriver.class).setup();
+        FirefoxOptions options = new FirefoxOptions();
         options.setHeadless(true);
-        options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
-        driver = new ChromeDriver(options);
+        driver = new FirefoxDriver(options);
         wait = new WebDriverWait(driver, 30);
+
+        InputStream inputStream = this.getClass().getResourceAsStream("/test.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        url = properties.getProperty("react.app.api").replace("/api", "");
     }
 
     @AfterEach
     public void tearDown() {
         driver.close();
-        driver.quit();
     }
 
     @Test
     public void registerTest() {
         //register
-        driver.get("http://localhost:8080/");
+        driver.get(url);
         driver.manage().window().setSize(new Dimension(1920, 1080));
         driver.findElement(By.id("register")).click();
         driver.findElement(By.id("username")).click();
