202fcfd5967f0ac5ad2829c2460ba1a01b8b455a
Jakub Bogdan <kubab1998@o2.pl>
2020-06-02 20:54:09 +0200

    Deleted unnecessary things

--- src/main/dogs-js/src/App.js
+++ src/main/dogs-js/src/App.js
@@ -7,7 +7,6 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import NotFound from "./components/NotFound";
 import EditAccount from "./components/EditAccount";
-import RandomDog from "./components/RandomDog";
 import RatedDogs from "./components/RatedDogs";
  import RandomBreedDog from "./components/RandomBreedDog";
 import FavoriteDogs from "./components/FavoriteDogs";
@@ -25,7 +24,6 @@ export default class App extends Component {
                             <Route exact path="/login" component={Login}/>
                             <Route exact path="/register" component={Register}/>
                             <Route exact path="/account" component={EditAccount}/>
-                            <Route exact path="/randomDog" component={RandomDog}/>
                             <Route exact path="/ratedDogs" component={RatedDogs}/>
                             <Route exact path="/randomBreedDog" component={RandomBreedDog}/>
                             <Route exact path="/favoriteDogs" component={FavoriteDogs} />
