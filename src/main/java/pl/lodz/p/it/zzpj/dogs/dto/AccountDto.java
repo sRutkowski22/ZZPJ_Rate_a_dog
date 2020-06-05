e1769681066b55426f9899c2aa18b5718e8be09d
Jakub Bogdan <kubab1998@o2.pl>
2020-06-05 20:04:06 +0200

    Cleaning database before every test

--- src/main/java/pl/lodz/p/it/zzpj/dogs/dto/AccountDto.java
+++ src/main/java/pl/lodz/p/it/zzpj/dogs/dto/AccountDto.java
@@ -3,8 +3,6 @@ package pl.lodz.p.it.zzpj.dogs.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.Map;
-
 @Builder
 public @Data class AccountDto {
 
--- src/test/java/pl/lodz/p/it/zzpj/dogs/AccountServiceTests.java
+++ src/test/java/pl/lodz/p/it/zzpj/dogs/AccountServiceTests.java
@@ -1,15 +1,13 @@
 package pl.lodz.p.it.zzpj.dogs;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import pl.lodz.p.it.zzpj.dogs.exceptions.AppBaseException;
 import pl.lodz.p.it.zzpj.dogs.model.Account;
+import pl.lodz.p.it.zzpj.dogs.repositories.AccountRepository;
 import pl.lodz.p.it.zzpj.dogs.services.AccountService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,21 +16,26 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 @SpringBootTest
 @Import(TestMongoConfiguration.class)
 @ExtendWith(TestSuiteExtension.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AccountServiceTests {
 
     @Autowired
-    AccountService accountService;
+    private AccountService accountService;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @BeforeEach
+    private void cleanDatabase() {
+        accountRepository.deleteAll();
+    }
 
     @Test
-    @Order(1)
     void getAllTest() {
         int size = accountService.getAll().size();
         assertEquals(0, size);
     }
 
     @Test
-    @Order(2)
     void addAccountTest() throws AppBaseException {
         int size = accountService.getAll().size();
         accountService.addAccount(Account.builder().username("addTest").password("password123").firstName("addTest").build());
@@ -41,7 +44,6 @@ public class AccountServiceTests {
     }
 
     @Test
-    @Order(3)
     void getAccountTest() throws AppBaseException {
         accountService.addAccount(Account.builder().username("getAccountTest").password("password123").firstName("getAccountTest").build());
         String name = accountService.getAccount("getAccountTest").getFirstName();
@@ -49,7 +51,6 @@ public class AccountServiceTests {
     }
 
     @Test
-    @Order(4)
     void editAccountTest() throws AppBaseException {
         Account account = Account.builder().username("editAccountTest").password("password123").firstName("editAccountTest").build();
         accountService.addAccount(account);
@@ -62,7 +63,6 @@ public class AccountServiceTests {
     }
 
     @Test
-    @Order(5)
     void addDogToFavoritesTest() throws AppBaseException {
         Account account = Account.builder().username("favorite").password("password123").firstName("favorite").build();
         accountService.addAccount(account);
@@ -73,7 +73,6 @@ public class AccountServiceTests {
     }
 
     @Test
-    @Order(6)
     void getFavoriteDogsTest() throws AppBaseException {
         Account account = Account.builder().username("favorites").password("password123").firstName("favorites").build();
         accountService.addAccount(account);
